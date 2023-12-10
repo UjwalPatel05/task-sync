947733f3fecc366fa2ece7aacbdf014b6ee67465
Ujwal Patel <updatel1757@conestogac.on.ca>
2023-12-10 13:52:56 -0500

    Deployment Fix

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,67 +1,84 @@
 "use client";
 
-import { copyCard } from "@/actions/copy-card";
-import { deleteCard } from "@/actions/delete-card";
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useAction } from "@/hooks/use-action";
-import { useCardModal } from "@/hooks/use-card-modal";
-import { CardWithList } from "@/types";
-import { CopyIcon, TrashIcon } from "lucide-react";
-import { useParams } from "next/navigation";
 import { toast } from "sonner";
+import { Copy, Trash } from "lucide-react";
+import { useParams } from "next/navigation";
+
+import { CardWithList } from "@/types";
+import { useAction } from "@/hooks/use-action";
+import { copyCard } from "@/actions/copy-card";
+import { Button } from "@/components/ui/button";
+import { deleteCard } from "@/actions/delete-card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface ActionsProps {
   data: CardWithList;
-}
+};
 
-// eslint-disable-next-line react/display-name
-const Actions = ({ data }: ActionsProps) => {
+export const Actions = ({
+  data,
+}: ActionsProps) => {
   const params = useParams();
   const cardModal = useCardModal();
 
-  const { execute: executeCopyCard, isLoading: isLoadingCopy } =
-    useAction(copyCard,{
-        onSuccess: (data) => {
-            toast.success(`Card "${data.title}" copied`);
-            cardModal.onClose();
-        },
-        onError: (error) => {
-            toast.error(error);
-        }
-    });
-  const { execute: executeDeleteCard, isLoading: isLoadingDelete } =
-    useAction(deleteCard,{
-        onSuccess: (data) => {
-            toast.success(`Card "${data.title}" deleted`);
-            cardModal.onClose();
-        },
-        onError: (error) => {
-            toast.error(error);
-        }
-    });
+  const { 
+    execute: executeCopyCard,
+    isLoading: isLoadingCopy,
+  } = useAction(copyCard, {
+    onSuccess: (data) => {
+      toast.success(`Card "${data.title}" copied`);
+      cardModal.onClose();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const { 
+    execute: executeDeleteCard,
+    isLoading: isLoadingDelete,
+  } = useAction(deleteCard, {
+    onSuccess: (data) => {
+      toast.success(`Card "${data.title}" deleted`);
+      cardModal.onClose();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
 
   const onCopy = () => {
     const boardId = params.boardId as string;
-    executeCopyCard({ boardId, id: data.id });
+
+    executeCopyCard({
+      id: data.id,
+      boardId,
+    });
   };
 
   const onDelete = () => {
     const boardId = params.boardId as string;
-    executeDeleteCard({ boardId, id: data.id });
-  };
 
+    executeDeleteCard({
+      id: data.id,
+      boardId,
+    });
+  };
+  
   return (
     <div className="space-y-2 mt-2">
-      <p className="text-xs font-semibold">Actions</p>
+      <p className="text-xs font-semibold">
+        Actions
+      </p>
       <Button
+        onClick={onCopy}
+        disabled={isLoadingCopy}
         variant="gray"
         className="w-full justify-start"
         size="inline"
-        onClick={onCopy}
-        disabled={isLoadingCopy}
       >
-        <CopyIcon className="h-4 w-4 mr-2" />
+        <Copy className="h-4 w-4 mr-2" />
         Copy
       </Button>
       <Button
@@ -71,23 +88,19 @@ const Actions = ({ data }: ActionsProps) => {
         className="w-full justify-start"
         size="inline"
       >
-        <TrashIcon className="h-4 w-4 mr-2" />
+        <Trash className="h-4 w-4 mr-2" />
         Delete
       </Button>
     </div>
   );
 };
 
-
-
-Actions.Skeleton = () => {
+Actions.Skeleton = function ActionsSkeleton() {
   return (
     <div className="space-y-2 mt-2">
-      <Skeleton className="h-4 w-20 bg-neutral-200" />
-      <Skeleton className="h-8 w-full bg-neutral-200" />
-      <Skeleton className="h-8 w-full bg-neutral-200" />
+      <Skeleton className="w-20 h-4 bg-neutral-200" />
+      <Skeleton className="w-full h-8 bg-neutral-200" />
+      <Skeleton className="w-full h-8 bg-neutral-200" />
     </div>
   );
 };
-
-export default Actions;
