33f34cc15fcd57594ff6fe7585107f8dcd8ca0c2
Ujwal Patel <updatel1757@conestogac.on.ca>
2023-12-26 15:19:30 -0500

    Deployment Change

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -38,15 +38,14 @@ export const Info = ({
   console.log("isLoaded:", isLoaded);
   console.log("organization:", organization);
 
-  // if (!isLoaded) {
-  //   return (
-  //     <Info.Skeleton />
-  //   );
-  // }
+  if (!isLoaded) {
+    return (
+      <Info.Skeleton />
+    );
+  }
 
   return (
-    <>
-    {/* <div className="flex items-center gap-x-4">
+    <div className="flex items-center gap-x-4">
       <div className="w-[60px] h-[60px] relative">
         <Image
           fill
@@ -64,28 +63,7 @@ export const Info = ({
           {isPro ? "Pro" : "Free"}
         </div>
       </div>
-    </div> */}
-
-    {!isLoaded?(<Info.Skeleton />):(<div className="flex items-center gap-x-4">
-      <div className="w-[60px] h-[60px] relative">
-        <Image
-          fill
-          src={organization?.imageUrl!}
-          alt="Organization"
-          className="rounded-md object-cover"
-        />
-      </div>
-      <div className="space-y-1">
-        <p className="font-semibold text-xl">
-          {organization?.name}
-        </p>
-        <div className="flex items-center text-xs text-muted-foreground">
-          <CreditCard className="h-3 w-3 mr-1" />
-          {isPro ? "Pro" : "Free"}
-        </div>
-      </div>
-    </div>)}
-    </>
+    </div>
   );
 };
 
